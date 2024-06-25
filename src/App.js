e61875cebeaa417627239efaebcac49ff29f8904
Yannie <k_t_dino@hotmail.com>
2024-06-25 14:57:15 +0800

    run test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Testing</div>;
+  return;
+  <div className="App">
+    <h1>Testing</h1>
+  </div>;
 }
 
 export default App;
